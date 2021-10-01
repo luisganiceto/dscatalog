1ebaf7a7efc168f3ab86cd01e88e275eddc69bfb
Luis Aniceto <luisg.aniceto@hotmail.com>
2021-10-01 17:47:10 -0300

    Find By Id

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,12 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
-import java.util.stream.Collector;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
@@ -17,11 +18,17 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
-		List<Category> list = repository.findAll();
-		
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+		List<Category> list = repository.findAll();		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());		
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> optional = repository.findById(id);
+		Category category = optional.get();		
+		return new CategoryDTO(category);
 	}
 	
 
