a021ad228468afb5fb358abae6106326f82a8fdb
Luis Aniceto <luisg.aniceto@hotmail.com>
2021-10-10 11:31:45 -0300

    Product Entity

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -10,8 +10,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 
 @Entity 
+@Table(name = "tb_category")
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -21,7 +23,7 @@ public class Category implements Serializable{
 	private String name;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant cretedAt;
+	private Instant createdAt;
 	
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
@@ -51,7 +53,7 @@ public class Category implements Serializable{
 	}
 
 	public Instant getCretedAt() {
-		return cretedAt;
+		return createdAt;
 	}
 
 	public Instant getUpdatedAt() {
@@ -60,7 +62,7 @@ public class Category implements Serializable{
 	
 	@PrePersist
 	public void prePersist() {
-		cretedAt = Instant.now();
+		createdAt = Instant.now();
 	}
 	
 	@PreUpdate
