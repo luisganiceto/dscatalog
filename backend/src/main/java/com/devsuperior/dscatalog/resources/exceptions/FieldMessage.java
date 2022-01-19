174bcfac6016b23b3236f221b68d5d080902629b
Luis Aniceto <luisg.aniceto@hotmail.com>
2022-01-18 21:06:01 -0300

    custom validation response

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/FieldMessage.java
@@ -0,0 +1,37 @@
+package com.devsuperior.dscatalog.resources.exceptions;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldName;
+	private String name;
+	
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String fieldName, String name) {
+		super();
+		this.fieldName = fieldName;
+		this.name = name;
+	}
+
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
--- backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,12 +33,28 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> database(DataBaseException e, HttpServletRequest request){
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.BAD_REQUEST.value());
-		error.setError("Deu Erro loko aqui");
+		error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		error.setError("Database Exception");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		ValidationError error = new ValidationError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		error.setError("Validation Exception");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			error.addError(f.getField(), f.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(error);
 	}
 	
 	
